069c69ded07c09f256bc4887ee0eb8b9d0e2735a
rpoliselit <38543059+rpoliselit@users.noreply.github.com>
2024-02-17 12:39:39 -0300

    Add a list of assets as example

---
+++ assets_list.py
@@ -0,0 +1,15 @@
+stocks_list = ['B3SA3',
+          'BBDC3',
+          'BBAS3',
+          'ITUB3',
+          'SANB3',
+          'ALUP3',
+          'TAEE3',
+          'TAEE4',
+          'TAEE11',
+          'CPLE3']
+          
+fiis_list = ['KNRI11',
+        'VGHF11',
+        'HSML11',
+        'HGLG11']
