a5def5927af0aa8cec352976cd5b400e44cdbaba
rpoliselit <38543059+rpoliselit@users.noreply.github.com>
2024-08-24 15:33:35 -0300

    Add "ibrx" and "idiv" indexes and prices

--- valuation.py
+++ valuation.py
@@ -31,8 +31,14 @@ class stocks:
     def __init__(self, assets = None):
         self.assets = assets
         self.indexes = {'Cotação', 'Div. Yield' ,'P/L', 'P/VP', 'VPA', 'LPA'}
-        self.data = {'Ações' : [], 'Preço Justo (Graham)' : [], 'Preço Justo (IBOVESPA)' : []}
+        self.data = {'Ações' : [], 'Preço Justo (Graham)' : [], 'Preço Justo (IBOVESPA)' : [], 'Preço Justo (IBRX)' : [], 'Preço Justo (IDIV)' : []}
         self.df = None
+        self.ibov_pl = None
+        self.ibov_pvp = None
+        self.ibrx_pl = None
+        self.ibrx_pvp = None
+        self.idiv_pl = None
+        self.idiv_pvp = None
 
     def ibovespa_statusinvest(self, show = False):
         url = 'https://statusinvest.com.br/indices/ibovespa'
@@ -41,7 +47,33 @@ class stocks:
         for index in indexes:
             indexes[index] = extract_index(index, soup, tag1 = 'h3', tag2 = 'strong')
         if show:
-            print("| IBOVESPA | P/L = ", indexes['P/L'], ' | P/VP = ', indexes['P/VP'], ' |')
+            self.ibov_pl = indexes['P/L']
+            self.ibov_pvp = indexes['P/VP']
+            print("|  IBOVESPA  | P/L = ", indexes['P/L'], ' | P/VP = ', indexes['P/VP'], ' |')
+        return indexes['P/L'] * indexes['P/VP']
+
+    def ibrx_statusinvest(self, show = False):
+        url = 'https://statusinvest.com.br/indices/indice-brasil-100'
+        indexes = {'P/L' : None, 'P/VP': None}
+        soup = request_data(url)
+        for index in indexes:
+            indexes[index] = extract_index(index, soup, tag1 = 'h3', tag2 = 'strong')
+        if show:
+            self.ibrx_pl = indexes['P/L']
+            self.ibrx_pvp = indexes['P/VP']
+            print("| BRASIL 100 | P/L = ", indexes['P/L'], ' | P/VP = ', indexes['P/VP'], ' |')
+        return indexes['P/L'] * indexes['P/VP']
+
+    def idiv_statusinvest(self, show = False):
+        url = 'https://statusinvest.com.br/indices/indice-dividendos'
+        indexes = {'P/L' : None, 'P/VP': None}
+        soup = request_data(url)
+        for index in indexes:
+            indexes[index] = extract_index(index, soup, tag1 = 'h3', tag2 = 'strong')
+        if show:
+            self.idiv_pl = indexes['P/L']
+            self.idiv_pvp = indexes['P/VP']
+            print("| DIVIDENDOS | P/L = ", indexes['P/L'], ' | P/VP = ', indexes['P/VP'], ' |')
         return indexes['P/L'] * indexes['P/VP']
 
     def intrinsic_value(self, LPA, VPA, BG = 22.5):
@@ -49,7 +81,7 @@ class stocks:
         # PVP == price to book value = 1.5
         # VPA == book value
         # LPA == EPS
-        # BG = PL * PVP
+        # BG = PL * PVP = 22.5
         price = math.sqrt(BG * LPA * VPA)
         return round(price, 2)
 
@@ -57,6 +89,8 @@ class stocks:
         LPA = None
         VPA = None
         IBOV = self.ibovespa_statusinvest(show=True)
+        IBRX = self.ibrx_statusinvest(show=True)
+        IDIV = self.idiv_statusinvest(show=True)
         for asset in self.assets:
             self.data['Ações'].append(asset)
             resp = request_from_fundamentus(asset)
@@ -69,8 +103,9 @@ class stocks:
                 VPA = index_value if index == 'VPA' else VPA
             self.data['Preço Justo (Graham)'].append(self.intrinsic_value(LPA, VPA))
             self.data['Preço Justo (IBOVESPA)'].append(self.intrinsic_value(LPA, VPA, BG = IBOV))
+            self.data['Preço Justo (IBRX)'].append(self.intrinsic_value(LPA, VPA, BG = IBRX))
+            self.data['Preço Justo (IDIV)'].append(self.intrinsic_value(LPA, VPA, BG = IDIV))
         self.df = pd.DataFrame(data=self.data)
-
 
     def info(self):
         self.make_dataframe()
